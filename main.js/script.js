44fed4ea5b1def2cf8fe0cb27de38f5152c57f85
Skye <Skye@supermind.us>
2023-10-09 18:28:17 -0400

    decreased amount of time mole is active, added play again button, added functionality for score to go down when you miss, attempted to add an end of game function based on the set timeout.

--- main.js/script.js
+++ main.js/script.js
@@ -17,10 +17,13 @@ function renderGame(){
     tile.addEventListener("click", clickedTile);
     }
 
-    setInterval(activateTile, 2000);
+    setInterval(activateTile, 1000);
     setInterval(setTimer, 1000);
+    setTimeout(endGame, 12000);
 }
-
+if (gameOver === false){
+    document.getElementById("playButton").style.display= 'transparent';
+}
 //randomly choose tile
 function randomTile(){
     let num = Math.floor(Math.random() * 9)
@@ -42,18 +45,36 @@ function activateTile() {
     activeTile = document.getElementById(num);
     activeTile.appendChild(mole);
 }   
-
+//generate points when mole clicked, lose points if you miss
 function clickedTile(){
     if (this === activeTile){
         score += 10;
         document.getElementById("score").innerHTML=score.toString();
     }
+    else {
+        score -= 10;
+        document.getElementById("score").innerHTML=score.toString();
+    }
 }
-
+//display timer on screen
 function setTimer(){
     timer += 1;
     document.getElementById("timer").innerText= "TIMER: " + timer.toString() + "s";
 }
+// win condition
+
+function endGame() {
+   document.querySelector(gameOver).innerText="true";
+     if(gameOver === "true"){
+      document.getElementById("playButton").addEventListener("click", renderGame());
+      document.getElementById("timer").innerText = 0; 
+      document.getElementById("score").innerText = 0;
+     }   
+    }
+
+
+
+
 //
 /*function missClick(){
     let missedClick = document.querySelector(onclick)
